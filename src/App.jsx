eab2d23219d400d4c5399bc078419c2e2be7a71e
Robert Ngo <robert.ngo@lacapitale.com>
2023-05-19 16:34:26 -0400

    Use environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import ParkingMap from './components/ParkingMap';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iZXJ0bmdvIiwiYSI6ImNsaHV3cjNnZDAzaDAzbGxpdzFnN2NxYzEifQ.ABlMviWnDpB-D4BJ96JXGA';
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 function App() {
   return (
     <ParkingMap />
