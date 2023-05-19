769be89b67a76146d5e7d0849b0267d22e5b0576
Robert Ngo <robert.ngo@lacapitale.com>
2023-05-19 16:29:55 -0400

    Clean up unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import ParkingMap from './components/ParkingMap';
-import './App.css';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iZXJ0bmdvIiwiYSI6ImNsaHV3cjNnZDAzaDAzbGxpdzFnN2NxYzEifQ.ABlMviWnDpB-D4BJ96JXGA';
 function App() {
   return (
-    <>
-      <h1>Map here</h1>
-      <ParkingMap />
-    </>
+    <ParkingMap />
   );
 }
 
