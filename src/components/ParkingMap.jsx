cfdaed114d1b1df9ca1843f0665a8037e4a7f2d0
Robert Ngo <robert.ngo@lacapitale.com>
2023-05-23 16:49:00 -0400

    Add neighbours

--- src/components/ParkingMap.jsx
+++ src/components/ParkingMap.jsx
@@ -1,16 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import Map, { Marker, Popup } from 'react-map-gl';
+import React, { useEffect, useState, useCallback } from 'react';
+import Map, {
+  Source, Layer, Marker, Popup,
+} from 'react-map-gl';
 import Pin from './Pin';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const ENDPOINT = 'https://montreal-parking.s3.ca-central-1.amazonaws.com/stationnements_h_2022_2023.geojson';
+const BASE_ENDPOINT = 'https://montreal-parking.s3.ca-central-1.amazonaws.com';
+const ENDPOINT_STATIONNEMENTS = `${BASE_ENDPOINT}/stationnements_h_2022_2023.geojson`;
+const ENDPOINT_LIMADMIN = `${BASE_ENDPOINT}/limadmin.geojson`;
 
 function ParkingMap() {
   const [pins, setPins] = useState([]);
   const [popupInfo, setPopupInfo] = useState(null);
 
+  const [neighbors, setNeighbors] = useState([]);
+  const [hoverInfo, setHoverInfo] = useState(null);
+
+  const onHover = useCallback((event) => {
+    const {
+      features,
+      point: { x, y },
+    } = event;
+
+    const hoveredFeature = features && features[0];
+
+    // prettier-ignore
+    setHoverInfo(hoveredFeature && { feature: hoveredFeature, x, y });
+  }, []);
+
   useEffect(() => {
-    fetch(ENDPOINT)
+    fetch(ENDPOINT_STATIONNEMENTS)
       .then((resp) => resp.json())
       .then((raw) => {
         const markers = raw.features.map(({ properties }) => (
@@ -30,6 +49,12 @@ function ParkingMap() {
         setPins(markers);
       })
       .catch((error) => console.error('Error loading data', error));
+
+    fetch(ENDPOINT_LIMADMIN)
+      .then((resp) => resp.json())
+      .then((raw) => {
+        setNeighbors(raw);
+      });
   }, []);
 
   return (
@@ -42,7 +67,29 @@ function ParkingMap() {
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       style={{ height: 600 }}
+      interactiveLayerIds={['neighbors']}
+      onMouseMove={onHover}
     >
+      <Source type="geojson" data={neighbors}>
+        <Layer
+          type="fill"
+          paint={{
+            'fill-opacity': 0.5, 'fill-color': 'transparent',
+          }}
+        />
+        <Layer
+          type="line"
+          paint={{
+            'line-color': '#000', 'line-width': 1,
+          }}
+        />
+      </Source>
+      {hoverInfo && (
+        <div className="toolTip">
+          <div>{hoverInfo.properties.NOM}</div>
+        </div>
+      )}
+
       {pins}
 
       {popupInfo && (
